288ec1608f3ec5510be900d9278f38c764256868
Brandon Fosdick <bfoz@bfoz.net>
2012-04-21 17:29:54 -0700

    Added a little bit of place holder rdoc

--- lib/engineering.rb
+++ lib/engineering.rb
@@ -3,6 +3,11 @@ require 'units'
 
 require_relative 'dxf'
 
+=begin
+A meta-gem for wayward engineering-related gems. Here you can find everything 
+you'll need for your latest engineering project.
+=end
+
 module Engineering
   # Your code goes here...
 end
