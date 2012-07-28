104056387613fef628e796a0d6f3edc0ec7c16c1
Brandon Fosdick <bfoz@bfoz.net>
2012-07-27 23:50:08 -0700

    Fixed Transformation generation when exporting to SketchUp

--- lib/sketchup.rb
+++ lib/sketchup.rb
@@ -109,7 +109,7 @@ module SketchUp
 		when Geometry::Rectangle
 		    "#{parent}.add_face(#{to_sketchup(entity.points)})"
 		when Geometry::Transformation
-		    pt = entity.translation ? to_sketchup(entity.translation) : ''
+		    pt = entity.translation ? to_sketchup(entity.translation) : '[0,0,0]'
 		    x_axis = '[' + (entity.x_axis ? to_sketchup(entity.x_axis) : '1,0,0') + ']'
 		    y_axis = '[' + (entity.y_axis ? to_sketchup(entity.y_axis) : '0,1,0') + ']'
 		    "Geom::Transformation.new(#{[pt,x_axis,y_axis].join(',')})"
